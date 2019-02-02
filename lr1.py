885dd30ee5171a897f5f4c65b89f6805791b97f0
Diego Amicabile <diego.amicabile@gmail.com>
2019-02-02 23:13:44 +0100

    lr2

--- lr1.py
+++ lr1.py
@@ -463,3 +463,364 @@ t_min_idx = np.unravel_index(np.argmin(J), J.shape)
 t1_min, t2_min = t1[t_min_idx], t2[t_min_idx]
 
 t_init = np.array([[0.25], [-1]])
+
+
+def bgd_path(theta, X, y, l1, l2, core=1, eta=0.1, n_iterations=50):
+    path = [theta]
+    for iteration in range(n_iterations):
+        gradients = core * 2 / len(X) * X.T.dot(X.dot(theta) - y) + l1 * np.sign(theta) + 2 * l2 * theta
+
+        theta = theta - eta * gradients
+        path.append(theta)
+    return np.array(path)
+
+
+plt.figure(figsize=(12, 8))
+for i, N, l1, l2, title in ((0, N1, 0.5, 0, "Lasso"), (1, N2, 0, 0.1, "Ridge")):
+    JR = J + l1 * N1 + l2 * N2 ** 2
+
+    tr_min_idx = np.unravel_index(np.argmin(JR), JR.shape)
+    t1r_min, t2r_min = t1[tr_min_idx], t2[tr_min_idx]
+
+    levelsJ = (np.exp(np.linspace(0, 1, 20)) - 1) * (np.max(J) - np.min(J)) + np.min(J)
+    levelsJR = (np.exp(np.linspace(0, 1, 20)) - 1) * (np.max(JR) - np.min(JR)) + np.min(JR)
+    levelsN = np.linspace(0, np.max(N), 10)
+
+    path_J = bgd_path(t_init, Xr, yr, l1=0, l2=0)
+    path_JR = bgd_path(t_init, Xr, yr, l1, l2)
+    path_N = bgd_path(t_init, Xr, yr, np.sign(l1) / 3, np.sign(l2), core=0)
+
+    plt.subplot(221 + i * 2)
+    plt.grid(True)
+    plt.axhline(y=0, color='k')
+    plt.axvline(x=0, color='k')
+    plt.contourf(t1, t2, J, levels=levelsJ, alpha=0.9)
+    plt.contour(t1, t2, N, levels=levelsN)
+    plt.plot(path_J[:, 0], path_J[:, 1], "w-o")
+    plt.plot(path_N[:, 0], path_N[:, 1], "y-^")
+    plt.plot(t1_min, t2_min, "rs")
+    plt.title(r"$\ell_{}$ penalty".format(i + 1), fontsize=16)
+    plt.axis([t1a, t1b, t2a, t2b])
+    if i == 1:
+        plt.xlabel(r"$\theta_1$", fontsize=20)
+    plt.ylabel(r"$\theta_2$", fontsize=20, rotation=0)
+
+    plt.subplot(222 + i * 2)
+    plt.grid(True)
+    plt.axhline(y=0, color='k')
+    plt.axvline(x=0, color='k')
+    plt.contourf(t1, t2, JR, levels=levelsJR, alpha=0.9)
+    plt.plot(path_JR[:, 0], path_JR[:, 1], "w-o")
+    plt.plot(t1r_min, t2r_min, "rs")
+    plt.title(title, fontsize=16)
+    plt.axis([t1a, t1b, t2a, t2b])
+    if i == 1:
+        plt.xlabel(r"$\theta_1$", fontsize=20)
+
+save_fig("lasso_vs_ridge_plot")
+plt.show()
+
+
+t = np.linspace(-10, 10, 100)
+sig = 1 / (1 + np.exp(-t))
+plt.figure(figsize=(9, 3))
+plt.plot([-10, 10], [0, 0], "k-")
+plt.plot([-10, 10], [0.5, 0.5], "k:")
+plt.plot([-10, 10], [1, 1], "k:")
+plt.plot([0, 0], [-1.1, 1.1], "k-")
+plt.plot(t, sig, "b-", linewidth=2, label=r"$\sigma(t) = \frac{1}{1 + e^{-t}}$")
+plt.xlabel("t")
+plt.legend(loc="upper left", fontsize=20)
+plt.axis([-10, 10, -0.1, 1.1])
+save_fig("logistic_function_plot")
+plt.show()
+
+from sklearn import datasets
+iris = datasets.load_iris()
+list(iris.keys())
+
+X = iris["data"][:, 3:]  # petal width
+y = (iris["target"] == 2).astype(np.int)  # 1 if I
+
+from sklearn.linear_model import LogisticRegression
+log_reg = LogisticRegression(random_state=42)
+log_reg.fit(X, y)
+
+X_new = np.linspace(0, 3, 1000).reshape(-1, 1)
+y_proba = log_reg.predict_proba(X_new)
+
+plt.plot(X_new, y_proba[:, 1], "g-", linewidth=2, label="Iris-Virginica")
+plt.plot(X_new, y_proba[:, 0], "b--", linewidth=2, label="Not Iris-Virginica")
+
+
+X_new = np.linspace(0, 3, 1000).reshape(-1, 1)
+y_proba = log_reg.predict_proba(X_new)
+decision_boundary = X_new[y_proba[:, 1] >= 0.5][0]
+
+plt.figure(figsize=(8, 3))
+plt.plot(X[y==0], y[y==0], "bs")
+plt.plot(X[y==1], y[y==1], "g^")
+plt.plot([decision_boundary, decision_boundary], [-1, 2], "k:", linewidth=2)
+plt.plot(X_new, y_proba[:, 1], "g-", linewidth=2, label="Iris-Virginica")
+plt.plot(X_new, y_proba[:, 0], "b--", linewidth=2, label="Not Iris-Virginica")
+plt.text(decision_boundary+0.02, 0.15, "Decision  boundary", fontsize=14, color="k", ha="center")
+plt.arrow(decision_boundary, 0.08, -0.3, 0, head_width=0.05, head_length=0.1, fc='b', ec='b')
+plt.arrow(decision_boundary, 0.92, 0.3, 0, head_width=0.05, head_length=0.1, fc='g', ec='g')
+plt.xlabel("Petal width (cm)", fontsize=14)
+plt.ylabel("Probability", fontsize=14)
+plt.legend(loc="center left", fontsize=14)
+plt.axis([0, 3, -0.02, 1.02])
+save_fig("logistic_regression_plot")
+plt.show()
+
+print(decision_boundary)
+
+log_reg.predict([[1.7], [1.5]])
+
+from sklearn.linear_model import LogisticRegression
+
+X = iris["data"][:, (2, 3)]  # petal length, petal width
+y = (iris["target"] == 2).astype(np.int)
+
+log_reg = LogisticRegression(C=10**10, random_state=42)
+log_reg.fit(X, y)
+
+x0, x1 = np.meshgrid(
+        np.linspace(2.9, 7, 500).reshape(-1, 1),
+        np.linspace(0.8, 2.7, 200).reshape(-1, 1),
+    )
+X_new = np.c_[x0.ravel(), x1.ravel()]
+
+y_proba = log_reg.predict_proba(X_new)
+
+plt.figure(figsize=(10, 4))
+plt.plot(X[y==0, 0], X[y==0, 1], "bs")
+plt.plot(X[y==1, 0], X[y==1, 1], "g^")
+
+zz = y_proba[:, 1].reshape(x0.shape)
+contour = plt.contour(x0, x1, zz, cmap=plt.cm.brg)
+
+
+left_right = np.array([2.9, 7])
+boundary = -(log_reg.coef_[0][0] * left_right + log_reg.intercept_[0]) / log_reg.coef_[0][1]
+
+plt.clabel(contour, inline=1, fontsize=12)
+plt.plot(left_right, boundary, "k--", linewidth=3)
+plt.text(3.5, 1.5, "Not Iris-Virginica", fontsize=14, color="b", ha="center")
+plt.text(6.5, 2.3, "Iris-Virginica", fontsize=14, color="g", ha="center")
+plt.xlabel("Petal length", fontsize=14)
+plt.ylabel("Petal width", fontsize=14)
+plt.axis([2.9, 7, 0.8, 2.7])
+save_fig("logistic_regression_contour_plot")
+plt.show()
+
+X = iris["data"][:, (2, 3)]  # petal length, petal width
+y = iris["target"]
+
+softmax_reg = LogisticRegression(multi_class="multinomial",solver="lbfgs", C=10, random_state=42)
+softmax_reg.fit(X, y)
+
+x0, x1 = np.meshgrid(
+        np.linspace(0, 8, 500).reshape(-1, 1),
+        np.linspace(0, 3.5, 200).reshape(-1, 1),
+    )
+X_new = np.c_[x0.ravel(), x1.ravel()]
+
+
+y_proba = softmax_reg.predict_proba(X_new)
+y_predict = softmax_reg.predict(X_new)
+
+zz1 = y_proba[:, 1].reshape(x0.shape)
+zz = y_predict.reshape(x0.shape)
+
+plt.figure(figsize=(10, 4))
+plt.plot(X[y==2, 0], X[y==2, 1], "g^", label="Iris-Virginica")
+plt.plot(X[y==1, 0], X[y==1, 1], "bs", label="Iris-Versicolor")
+plt.plot(X[y==0, 0], X[y==0, 1], "yo", label="Iris-Setosa")
+
+from matplotlib.colors import ListedColormap
+custom_cmap = ListedColormap(['#fafab0','#9898ff','#a0faa0'])
+
+plt.contourf(x0, x1, zz, cmap=custom_cmap)
+contour = plt.contour(x0, x1, zz1, cmap=plt.cm.brg)
+plt.clabel(contour, inline=1, fontsize=12)
+plt.xlabel("Petal length", fontsize=14)
+plt.ylabel("Petal width", fontsize=14)
+plt.legend(loc="center left", fontsize=14)
+plt.axis([0, 7, 0, 3.5])
+save_fig("softmax_regression_contour_plot")
+plt.show()
+
+print(softmax_reg.predict([[5, 2]]))
+print(softmax_reg.predict_proba([[5, 2]]))
+
+X = iris["data"][:, (2, 3)]  # petal length, petal width
+y = iris["target"]
+
+X_with_bias = np.c_[np.ones([len(X), 1]), X]
+
+test_ratio = 0.2
+validation_ratio = 0.2
+total_size = len(X_with_bias)
+
+test_size = int(total_size * test_ratio)
+validation_size = int(total_size * validation_ratio)
+train_size = total_size - test_size - validation_size
+
+rnd_indices = np.random.permutation(total_size)
+
+X_train = X_with_bias[rnd_indices[:train_size]]
+y_train = y[rnd_indices[:train_size]]
+X_valid = X_with_bias[rnd_indices[train_size:-test_size]]
+y_valid = y[rnd_indices[train_size:-test_size]]
+X_test = X_with_bias[rnd_indices[-test_size:]]
+y_test = y[rnd_indices[-test_size:]]
+
+def to_one_hot(y):
+    n_classes = y.max() + 1
+    m = len(y)
+    Y_one_hot = np.zeros((m, n_classes))
+    Y_one_hot[np.arange(m), y] = 1
+    return Y_one_hot
+
+Y_train_one_hot = to_one_hot(y_train)
+Y_valid_one_hot = to_one_hot(y_valid)
+Y_test_one_hot = to_one_hot(y_test)
+
+def softmax(logits):
+    exps = np.exp(logits)
+    exp_sums = np.sum(exps, axis=1, keepdims=True)
+    return exps / exp_sums
+
+n_inputs = X_train.shape[1] # == 3 (2 features plus the bias term)
+n_outputs = len(np.unique(y_train))   # == 3 (3 iris classes)
+
+eta = 0.01
+n_iterations = 5001
+m = len(X_train)
+epsilon = 1e-7
+
+Theta = np.random.randn(n_inputs, n_outputs)
+
+for iteration in range(n_iterations):
+    logits = X_train.dot(Theta)
+    Y_proba = softmax(logits)
+    loss = -np.mean(np.sum(Y_train_one_hot * np.log(Y_proba + epsilon), axis=1))
+    error = Y_proba - Y_train_one_hot
+    if iteration % 500 == 0:
+        print(iteration, loss)
+    gradients = 1/m * X_train.T.dot(error)
+    Theta = Theta - eta * gradients
+
+logits = X_valid.dot(Theta)
+Y_proba = softmax(logits)
+y_predict = np.argmax(Y_proba, axis=1)
+
+accuracy_score = np.mean(y_predict == y_valid)
+print(accuracy_score)
+
+eta = 0.1
+n_iterations = 5001
+m = len(X_train)
+epsilon = 1e-7
+alpha = 0.1  # regularization hyperparameter
+
+Theta = np.random.randn(n_inputs, n_outputs)
+
+for iteration in range(n_iterations):
+    logits = X_train.dot(Theta)
+    Y_proba = softmax(logits)
+    xentropy_loss = -np.mean(np.sum(Y_train_one_hot * np.log(Y_proba + epsilon), axis=1))
+    l2_loss = 1/2 * np.sum(np.square(Theta[1:]))
+    loss = xentropy_loss + alpha * l2_loss
+    error = Y_proba - Y_train_one_hot
+    if iteration % 500 == 0:
+        print(iteration, loss)
+    gradients = 1/m * X_train.T.dot(error) + np.r_[np.zeros([1, n_outputs]), alpha * Theta[1:]]
+    Theta = Theta - eta * gradients
+
+logits = X_valid.dot(Theta)
+Y_proba = softmax(logits)
+y_predict = np.argmax(Y_proba, axis=1)
+
+accuracy_score = np.mean(y_predict == y_valid)
+print(accuracy_score)
+
+
+eta = 0.1
+n_iterations = 5001
+m = len(X_train)
+epsilon = 1e-7
+alpha = 0.1  # regularization hyperparameter
+best_loss = np.infty
+
+Theta = np.random.randn(n_inputs, n_outputs)
+
+for iteration in range(n_iterations):
+    logits = X_train.dot(Theta)
+    Y_proba = softmax(logits)
+    xentropy_loss = -np.mean(np.sum(Y_train_one_hot * np.log(Y_proba + epsilon), axis=1))
+    l2_loss = 1/2 * np.sum(np.square(Theta[1:]))
+    loss = xentropy_loss + alpha * l2_loss
+    error = Y_proba - Y_train_one_hot
+    gradients = 1/m * X_train.T.dot(error) + np.r_[np.zeros([1, n_outputs]), alpha * Theta[1:]]
+    Theta = Theta - eta * gradients
+
+    logits = X_valid.dot(Theta)
+    Y_proba = softmax(logits)
+    xentropy_loss = -np.mean(np.sum(Y_valid_one_hot * np.log(Y_proba + epsilon), axis=1))
+    l2_loss = 1/2 * np.sum(np.square(Theta[1:]))
+    loss = xentropy_loss + alpha * l2_loss
+    if iteration % 500 == 0:
+        print(iteration, loss)
+    if loss < best_loss:
+        best_loss = loss
+    else:
+        print(iteration - 1, best_loss)
+        print(iteration, loss, "early stopping!")
+        break
+
+logits = X_valid.dot(Theta)
+Y_proba = softmax(logits)
+y_predict = np.argmax(Y_proba, axis=1)
+
+accuracy_score = np.mean(y_predict == y_valid)
+print(accuracy_score)
+
+x0, x1 = np.meshgrid(
+        np.linspace(0, 8, 500).reshape(-1, 1),
+        np.linspace(0, 3.5, 200).reshape(-1, 1),
+    )
+X_new = np.c_[x0.ravel(), x1.ravel()]
+X_new_with_bias = np.c_[np.ones([len(X_new), 1]), X_new]
+
+logits = X_new_with_bias.dot(Theta)
+Y_proba = softmax(logits)
+y_predict = np.argmax(Y_proba, axis=1)
+
+zz1 = Y_proba[:, 1].reshape(x0.shape)
+zz = y_predict.reshape(x0.shape)
+
+plt.figure(figsize=(10, 4))
+plt.plot(X[y==2, 0], X[y==2, 1], "g^", label="Iris-Virginica")
+plt.plot(X[y==1, 0], X[y==1, 1], "bs", label="Iris-Versicolor")
+plt.plot(X[y==0, 0], X[y==0, 1], "yo", label="Iris-Setosa")
+
+from matplotlib.colors import ListedColormap
+custom_cmap = ListedColormap(['#fafab0','#9898ff','#a0faa0'])
+
+plt.contourf(x0, x1, zz, cmap=custom_cmap)
+contour = plt.contour(x0, x1, zz1, cmap=plt.cm.brg)
+plt.clabel(contour, inline=1, fontsize=12)
+plt.xlabel("Petal length", fontsize=14)
+plt.ylabel("Petal width", fontsize=14)
+plt.legend(loc="upper left", fontsize=14)
+plt.axis([0, 7, 0, 3.5])
+plt.show()
+
+logits = X_test.dot(Theta)
+Y_proba = softmax(logits)
+y_predict = np.argmax(Y_proba, axis=1)
+
+accuracy_score = np.mean(y_predict == y_test)
+print(accuracy_score)
