09b956e3f79e7cd0dc3ec217ccd77b4a6276b3a5
Diego Amicabile <diego.amicabile@gmail.com>
2019-02-02 23:03:08 +0100

    lr again

--- lr1.py
+++ lr1.py
@@ -70,3 +70,396 @@ from sklearn.linear_model import LinearRegression
 lin_reg = LinearRegression()
 lin_reg.fit(X, y)
 lin_reg.intercept_, lin_reg.coef_
+
+lin_reg.predict(X_new)
+
+theta_best_svd, residuals, rank, s = np.linalg.lstsq(X_b, y, rcond=1e-6)
+
+np.linalg.pinv(X_b).dot(y)
+
+eta = 0.1
+n_iterations = 1000
+m = 100
+theta = np.random.randn(2,1)
+
+for iteration in range(n_iterations):
+    gradients = 2/m * X_b.T.dot(X_b.dot(theta) - y)
+    theta = theta - eta * gradients
+
+print(theta)
+print(X_new_b.dot(theta))
+
+
+theta_path_bgd = []
+
+def plot_gradient_descent(theta, eta, theta_path=None):
+    m = len(X_b)
+    plt.plot(X, y, "b.")
+    n_iterations = 1000
+    for iteration in range(n_iterations):
+        if iteration < 10:
+            y_predict = X_new_b.dot(theta)
+            style = "b-" if iteration > 0 else "r--"
+            plt.plot(X_new, y_predict, style)
+        gradients = 2/m * X_b.T.dot(X_b.dot(theta) - y)
+        theta = theta - eta * gradients
+        if theta_path is not None:
+            theta_path.append(theta)
+    plt.xlabel("$x_1$", fontsize=18)
+    plt.axis([0, 2, 0, 15])
+    plt.title(r"$\eta = {}$".format(eta), fontsize=16)
+
+
+np.random.seed(42)
+theta = np.random.randn(2,1)  # random initialization
+
+plt.figure(figsize=(10,4))
+plt.subplot(131); plot_gradient_descent(theta, eta=0.02)
+plt.ylabel("$y$", rotation=0, fontsize=18)
+plt.subplot(132); plot_gradient_descent(theta, eta=0.1, theta_path=theta_path_bgd)
+plt.subplot(133); plot_gradient_descent(theta, eta=0.5)
+
+save_fig("gradient_descent_plot")
+plt.show()
+
+theta_path_sgd = []
+m = len(X_b)
+np.random.seed(42)
+
+n_epochs = 50
+t0, t1 = 5, 50  # learning schedule hyperparameters
+
+def learning_schedule(t):
+    return t0 / (t + t1)
+
+theta = np.random.randn(2,1)  # random initialization
+
+for epoch in range(n_epochs):
+    for i in range(m):
+        if epoch == 0 and i < 20:                    # not shown in the book
+            y_predict = X_new_b.dot(theta)           # not shown
+            style = "b-" if i > 0 else "r--"         # not shown
+            plt.plot(X_new, y_predict, style)        # not shown
+        random_index = np.random.randint(m)
+        xi = X_b[random_index:random_index+1]
+        yi = y[random_index:random_index+1]
+        gradients = 2 * xi.T.dot(xi.dot(theta) - yi)
+        eta = learning_schedule(epoch * m + i)
+        theta = theta - eta * gradients
+        theta_path_sgd.append(theta)                 # not shown
+
+plt.plot(X, y, "b.")                                 # not shown
+plt.xlabel("$x_1$", fontsize=18)                     # not shown
+plt.ylabel("$y$", rotation=0, fontsize=18)           # not shown
+plt.axis([0, 2, 0, 15])                              # not shown
+save_fig("sgd_plot")                                 # not shown
+plt.show()
+
+print(theta)
+
+from sklearn.linear_model import SGDRegressor
+sgd_reg = SGDRegressor(max_iter=50, penalty=None, eta0=0.1, random_state=42)
+sgd_reg.fit(X, y.ravel())
+
+print(sgd_reg.intercept_, sgd_reg.coef_)
+
+theta_path_mgd = []
+
+n_iterations = 50
+minibatch_size = 20
+
+np.random.seed(42)
+theta = np.random.randn(2,1)  # random initialization
+
+t0, t1 = 200, 1000
+def learning_schedule(t):
+    return t0 / (t + t1)
+
+t = 0
+for epoch in range(n_iterations):
+    shuffled_indices = np.random.permutation(m)
+    X_b_shuffled = X_b[shuffled_indices]
+    y_shuffled = y[shuffled_indices]
+    for i in range(0, m, minibatch_size):
+        t += 1
+        xi = X_b_shuffled[i:i+minibatch_size]
+        yi = y_shuffled[i:i+minibatch_size]
+        gradients = 2/minibatch_size * xi.T.dot(xi.dot(theta) - yi)
+        eta = learning_schedule(t)
+        theta = theta - eta * gradients
+        theta_path_mgd.append(theta)
+
+
+theta_path_bgd = np.array(theta_path_bgd)
+theta_path_sgd = np.array(theta_path_sgd)
+theta_path_mgd = np.array(theta_path_mgd)
+
+plt.figure(figsize=(7,4))
+plt.plot(theta_path_sgd[:, 0], theta_path_sgd[:, 1], "r-s", linewidth=1, label="Stochastic")
+plt.plot(theta_path_mgd[:, 0], theta_path_mgd[:, 1], "g-+", linewidth=2, label="Mini-batch")
+plt.plot(theta_path_bgd[:, 0], theta_path_bgd[:, 1], "b-o", linewidth=3, label="Batch")
+plt.legend(loc="upper left", fontsize=16)
+plt.xlabel(r"$\theta_0$", fontsize=20)
+plt.ylabel(r"$\theta_1$   ", fontsize=20, rotation=0)
+
+import numpy as np
+import numpy.random as rnd
+
+np.random.seed(42)
+
+m = 100
+X = 6 * np.random.rand(m, 1) - 3
+y = 0.5 * X**2 + X + 2 + np.random.randn(m, 1)
+
+plt.plot(X, y, "b.")
+plt.xlabel("$x_1$", fontsize=18)
+plt.ylabel("$y$", rotation=0, fontsize=18)
+plt.axis([-3, 3, 0, 10])
+save_fig("quadratic_data_plot")
+plt.show()
+
+from sklearn.preprocessing import PolynomialFeatures
+poly_features = PolynomialFeatures(degree=2, include_bias=False)
+X_poly = poly_features.fit_transform(X)
+
+print(X[0])
+print(X_poly[0])
+
+lin_reg = LinearRegression()
+lin_reg.fit(X_poly, y)
+lin_reg.intercept_, lin_reg.coef_
+
+X_new=np.linspace(-3, 3, 100).reshape(100, 1)
+X_new_poly = poly_features.transform(X_new)
+y_new = lin_reg.predict(X_new_poly)
+plt.plot(X, y, "b.")
+plt.plot(X_new, y_new, "r-", linewidth=2, label="Predictions")
+plt.xlabel("$x_1$", fontsize=18)
+plt.ylabel("$y$", rotation=0, fontsize=18)
+plt.legend(loc="upper left", fontsize=14)
+plt.axis([-3, 3, 0, 10])
+save_fig("quadratic_predictions_plot")
+plt.show()
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+
+for style, width, degree in (("g-", 1, 300), ("b--", 2, 2), ("r-+", 2, 1)):
+    polybig_features = PolynomialFeatures(degree=degree, include_bias=False)
+    std_scaler = StandardScaler()
+    lin_reg = LinearRegression()
+    polynomial_regression = Pipeline([
+            ("poly_features", polybig_features),
+            ("std_scaler", std_scaler),
+            ("lin_reg", lin_reg),
+        ])
+    polynomial_regression.fit(X, y)
+    y_newbig = polynomial_regression.predict(X_new)
+    plt.plot(X_new, y_newbig, style, label=str(degree), linewidth=width)
+
+plt.plot(X, y, "b.", linewidth=3)
+plt.legend(loc="upper left")
+plt.xlabel("$x_1$", fontsize=18)
+plt.ylabel("$y$", rotation=0, fontsize=18)
+plt.axis([-3, 3, 0, 10])
+save_fig("high_degree_polynomials_plot")
+plt.show()
+
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+
+def plot_learning_curves(model, X, y):
+    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=10)
+    train_errors, val_errors = [], []
+    for m in range(1, len(X_train)):
+        model.fit(X_train[:m], y_train[:m])
+        y_train_predict = model.predict(X_train[:m])
+        y_val_predict = model.predict(X_val)
+        train_errors.append(mean_squared_error(y_train[:m], y_train_predict))
+        val_errors.append(mean_squared_error(y_val, y_val_predict))
+
+    plt.plot(np.sqrt(train_errors), "r-+", linewidth=2, label="train")
+    plt.plot(np.sqrt(val_errors), "b-", linewidth=3, label="val")
+    plt.legend(loc="upper right", fontsize=14)   # not shown in the book
+    plt.xlabel("Training set size", fontsize=14) # not shown
+    plt.ylabel("RMSE", fontsize=14)              # not shown
+
+lin_reg = LinearRegression()
+plot_learning_curves(lin_reg, X, y)
+plt.axis([0, 80, 0, 3])                         # not shown in the book
+save_fig("underfitting_learning_curves_plot")   # not shown
+plt.show()
+
+from sklearn.pipeline import Pipeline
+
+polynomial_regression = Pipeline([
+        ("poly_features", PolynomialFeatures(degree=10, include_bias=False)),
+        ("lin_reg", LinearRegression()),
+    ])
+
+plot_learning_curves(polynomial_regression, X, y)
+plt.axis([0, 80, 0, 3])           # not shown
+save_fig("learning_curves_plot")  # not shown
+plt.show()                        # not shown
+
+from sklearn.linear_model import Ridge
+
+np.random.seed(42)
+m = 20
+X = 3 * np.random.rand(m, 1)
+y = 1 + 0.5 * X + np.random.randn(m, 1) / 1.5
+X_new = np.linspace(0, 3, 100).reshape(100, 1)
+
+def plot_model(model_class, polynomial, alphas, **model_kargs):
+    for alpha, style in zip(alphas, ("b-", "g--", "r:")):
+        model = model_class(alpha, **model_kargs) if alpha > 0 else LinearRegression()
+        if polynomial:
+            model = Pipeline([
+                    ("poly_features", PolynomialFeatures(degree=10, include_bias=False)),
+                    ("std_scaler", StandardScaler()),
+                    ("regul_reg", model),
+                ])
+        model.fit(X, y)
+        y_new_regul = model.predict(X_new)
+        lw = 2 if alpha > 0 else 1
+        plt.plot(X_new, y_new_regul, style, linewidth=lw, label=r"$\alpha = {}$".format(alpha))
+    plt.plot(X, y, "b.", linewidth=3)
+    plt.legend(loc="upper left", fontsize=15)
+    plt.xlabel("$x_1$", fontsize=18)
+    plt.axis([0, 3, 0, 4])
+
+plt.figure(figsize=(8,4))
+plt.subplot(121)
+plot_model(Ridge, polynomial=False, alphas=(0, 10, 100), random_state=42)
+plt.ylabel("$y$", rotation=0, fontsize=18)
+plt.subplot(122)
+plot_model(Ridge, polynomial=True, alphas=(0, 10**-5, 1), random_state=42)
+
+save_fig("ridge_regression_plot")
+plt.show()
+
+from sklearn.linear_model import Ridge
+ridge_reg = Ridge(alpha=1, solver="cholesky", random_state=42)
+ridge_reg.fit(X, y)
+ridge_reg.predict([[1.5]])
+
+sgd_reg = SGDRegressor(max_iter=5, penalty="l2", random_state=42)
+sgd_reg.fit(X, y.ravel())
+sgd_reg.predict([[1.5]])
+
+ridge_reg = Ridge(alpha=1, solver="sag", random_state=42)
+ridge_reg.fit(X, y)
+ridge_reg.predict([[1.5]])
+
+from sklearn.linear_model import Lasso
+
+plt.figure(figsize=(8,4))
+plt.subplot(121)
+plot_model(Lasso, polynomial=False, alphas=(0, 0.1, 1), random_state=42)
+plt.ylabel("$y$", rotation=0, fontsize=18)
+plt.subplot(122)
+plot_model(Lasso, polynomial=True, alphas=(0, 10**-7, 1), tol=1, random_state=42)
+
+save_fig("lasso_regression_plot")
+plt.show()
+
+from sklearn.linear_model import Lasso
+lasso_reg = Lasso(alpha=0.1)
+lasso_reg.fit(X, y)
+lasso_reg.predict([[1.5]])
+
+from sklearn.linear_model import ElasticNet
+elastic_net = ElasticNet(alpha=0.1, l1_ratio=0.5, random_state=42)
+elastic_net.fit(X, y)
+elastic_net.predict([[1.5]])
+
+np.random.seed(42)
+m = 100
+X = 6 * np.random.rand(m, 1) - 3
+y = 2 + X + 0.5 * X**2 + np.random.randn(m, 1)
+
+X_train, X_val, y_train, y_val = train_test_split(X[:50], y[:50].ravel(), test_size=0.5, random_state=10)
+
+poly_scaler = Pipeline([
+        ("poly_features", PolynomialFeatures(degree=90, include_bias=False)),
+        ("std_scaler", StandardScaler()),
+    ])
+
+X_train_poly_scaled = poly_scaler.fit_transform(X_train)
+X_val_poly_scaled = poly_scaler.transform(X_val)
+
+sgd_reg = SGDRegressor(max_iter=1,
+                       penalty=None,
+                       eta0=0.0005,
+                       warm_start=True,
+                       learning_rate="constant",
+                       random_state=42)
+
+n_epochs = 500
+train_errors, val_errors = [], []
+for epoch in range(n_epochs):
+    sgd_reg.fit(X_train_poly_scaled, y_train)
+    y_train_predict = sgd_reg.predict(X_train_poly_scaled)
+    y_val_predict = sgd_reg.predict(X_val_poly_scaled)
+    train_errors.append(mean_squared_error(y_train, y_train_predict))
+    val_errors.append(mean_squared_error(y_val, y_val_predict))
+
+best_epoch = np.argmin(val_errors)
+best_val_rmse = np.sqrt(val_errors[best_epoch])
+
+plt.annotate('Best model',
+             xy=(best_epoch, best_val_rmse),
+             xytext=(best_epoch, best_val_rmse + 1),
+             ha="center",
+             arrowprops=dict(facecolor='black', shrink=0.05),
+             fontsize=16,
+            )
+
+best_val_rmse -= 0.03  # just to make the graph look better
+plt.plot([0, n_epochs], [best_val_rmse, best_val_rmse], "k:", linewidth=2)
+plt.plot(np.sqrt(val_errors), "b-", linewidth=3, label="Validation set")
+plt.plot(np.sqrt(train_errors), "r--", linewidth=2, label="Training set")
+plt.legend(loc="upper right", fontsize=14)
+plt.xlabel("Epoch", fontsize=14)
+plt.ylabel("RMSE", fontsize=14)
+save_fig("early_stopping_plot")
+plt.show()
+
+from sklearn.base import clone
+sgd_reg = SGDRegressor(max_iter=1, warm_start=True, penalty=None,
+                       learning_rate="constant", eta0=0.0005, random_state=42)
+
+minimum_val_error = float("inf")
+best_epoch = None
+best_model = None
+for epoch in range(1000):
+    sgd_reg.fit(X_train_poly_scaled, y_train)  # continues where it left off
+    y_val_predict = sgd_reg.predict(X_val_poly_scaled)
+    val_error = mean_squared_error(y_val, y_val_predict)
+    if val_error < minimum_val_error:
+        minimum_val_error = val_error
+        best_epoch = epoch
+        best_model = clone(sgd_reg)
+
+print(best_epoch, best_model)
+
+
+t1a, t1b, t2a, t2b = -1, 3, -1.5, 1.5
+
+# ignoring bias term
+t1s = np.linspace(t1a, t1b, 500)
+t2s = np.linspace(t2a, t2b, 500)
+t1, t2 = np.meshgrid(t1s, t2s)
+T = np.c_[t1.ravel(), t2.ravel()]
+Xr = np.array([[-1, 1], [-0.3, -1], [1, 0.1]])
+yr = 2 * Xr[:, :1] + 0.5 * Xr[:, 1:]
+
+J = (1/len(Xr) * np.sum((T.dot(Xr.T) - yr.T)**2, axis=1)).reshape(t1.shape)
+
+N1 = np.linalg.norm(T, ord=1, axis=1).reshape(t1.shape)
+N2 = np.linalg.norm(T, ord=2, axis=1).reshape(t1.shape)
+
+t_min_idx = np.unravel_index(np.argmin(J), J.shape)
+t1_min, t2_min = t1[t_min_idx], t2[t_min_idx]
+
+t_init = np.array([[0.25], [-1]])
